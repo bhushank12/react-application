f28b7b2edd02dea056bcc844658a124d559cb413
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-11-29 13:03:36 +0530

    First commit react basic application

---
+++ src/components/MyApplication.js
@@ -0,0 +1,10 @@
+function MyApplication() {
+  return (
+    <div>
+      <h1>Welcome to My Application</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+}
+
+export default MyApplication;
