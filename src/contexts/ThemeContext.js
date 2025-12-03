62cb0578f62b71082a4f98a05f5aecc205b058cf
Bhushan Kalode <bhushan.kalode@inkind.com>
2025-12-03 12:29:05 +0530

    Handle error for theme context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -16,4 +16,12 @@ export const ThemeProvider = ({ children }) => {
   );
 };
 
-export const useTheme = () => useContext(ThemeContext);
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if(!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
+
+export default ThemeContext;
